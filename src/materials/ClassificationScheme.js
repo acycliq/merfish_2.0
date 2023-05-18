8410c6a13c07cfec4c922a008ca6966d2a8b407c
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2023-05-18 12:59:05 +0100

    added the glyphs

--- src/materials/ClassificationScheme.js
+++ src/materials/ClassificationScheme.js
@@ -3,19 +3,36 @@ import * as THREE from "../../libs/three.js/build/three.module.js";
 
 export const ClassificationScheme = {
 
+// 	DEFAULT: {
+// 		0: {visible: true, name: 'never classified', color: [0.5, 0.5, 0.5, 1.0]},
+// 		1: {visible: true, name: 'unclassified', color: [0.5, 0.5, 0.5, 1.0]},
+// 		2: {visible: true, name: 'ground', color: [0.63, 0.32, 0.18, 1.0]},
+// 		3: {visible: true, name: 'low vegetation', color: [0.0, 1.0, 0.0, 1.0]},
+// 		4: {visible: true, name: 'medium vegetation', color: [0.0, 0.8, 0.0, 1.0]},
+// 		5: {visible: true, name: 'high vegetation', color: [0.0, 0.6, 0.0, 1.0]},
+// 		6: {visible: true, name: 'building', color: [1.0, 0.66, 0.0, 1.0]},
+// 		7: {visible: true, name: 'low point(noise)', color: [1.0, 0.0, 1.0, 1.0]},
+// 		8: {visible: true, name: 'key-point', color: [1.0, 0.0, 0.0, 1.0]},
+// 		9: {visible: true, name: 'water', color: [0.0, 0.0, 1.0, 1.0]},
+// 		12: {visible: true, name: 'overlap', color: [1.0, 1.0, 0.0, 1.0]},
+// 		DEFAULT: {visible: true, name: 'default', color: [0.3, 0.6, 0.6, 0.5]},
+// 	}
+// };
 	DEFAULT: {
-		0:       { visible: true, name: 'never classified'  , color: [0.5,  0.5,  0.5,  1.0] },
-		1:       { visible: true, name: 'unclassified'      , color: [0.5,  0.5,  0.5,  1.0] },
-		2:       { visible: true, name: 'ground'            , color: [0.63, 0.32, 0.18, 1.0] },
-		3:       { visible: true, name: 'low vegetation'    , color: [0.0,  1.0,  0.0,  1.0] },
-		4:       { visible: true, name: 'medium vegetation' , color: [0.0,  0.8,  0.0,  1.0] },
-		5:       { visible: true, name: 'high vegetation'   , color: [0.0,  0.6,  0.0,  1.0] },
-		6:       { visible: true, name: 'building'          , color: [1.0,  0.66, 0.0,  1.0] },
-		7:       { visible: true, name: 'low point(noise)'  , color: [1.0,  0.0,  1.0,  1.0] },
-		8:       { visible: true, name: 'key-point'         , color: [1.0,  0.0,  0.0,  1.0] },
-		9:       { visible: true, name: 'water'             , color: [0.0,  0.0,  1.0,  1.0] },
-		12:      { visible: true, name: 'overlap'           , color: [1.0,  1.0,  0.0,  1.0] },
-		DEFAULT: { visible: true, name: 'default'           , color: [0.3,  0.6,  0.6,  0.5] },
+		0: {visible: true, name: 'asterisk', color: [0.3, 0.6, 0.6, 0.5]},
+		1: {visible: true, name: 'circle', color: [0.3, 0.6, 0.6, 0.5]},
+		2: {visible: true, name: 'cross', color: [0.3, 0.6, 0.6, 0.5]},
+		3: {visible: true, name: 'diamond', color: [0.3, 0.6, 0.6, 0.5]},
+		4: {visible: true, name: 'plus', color: [0.3, 0.6, 0.6, 0.5]},
+		5: {visible: true, name: 'point', color: [0.3, 0.6, 0.6, 0.5]},
+		6: {visible: true, name: 'square', color: [0.3, 0.6, 0.6, 0.5]},
+		7: {visible: true, name: 'star5', color: [0.3, 0.6, 0.6, 0.5]},
+		8: {visible: true, name: 'star6', color: [0.3, 0.6, 0.6, 0.5]},
+		9: {visible: true, name: 'triangleDown', color: [0.3, 0.6, 0.6, 0.5]},
+		10: {visible: true, name: 'triangleLeft', color: [0.3, 0.6, 0.6, 0.5]},
+		11: {visible: true, name: 'triangleRight', color: [0.3, 0.6, 0.6, 0.5]},
+		12: {visible: true, name: 'triangleUp', color: [0.3, 0.6, 0.6, 0.5]},
+		DEFAULT: {visible: true, name: 'default', color: [0.3, 0.6, 0.6, 0.5]}
 	}
 };
 
--- src/materials/shaders/pointcloud.fs
+++ src/materials/shaders/pointcloud.fs
@@ -77,9 +77,188 @@ void main() {
             if(cc > 1.0){
                 discard;
             }
-            if(cc < vClassification/10.0){
-                discard;
+            if(vClassification == 1.0){
+            // draw the circle glyph
+                if(cc < 0.65){
+                    discard;
+                }
             }
+
+            if (vClassification == 2.0){
+            // draw the cross glyph
+                float diag_1 = 1.0 - lineSegment(gl_PointCoord, vec2(0.05, 0.05), vec2(0.95, 0.95));
+                float diag_2 = 1.0 - lineSegment(gl_PointCoord, vec2(0.05, 0.95), vec2(0.95, 0.05));
+                float shaper = diag_1 + diag_2;
+                gl_FragColor = vec4(vec3(shaper) * vColor, gl_FragColor.a);
+                if (gl_FragColor.r + gl_FragColor.g + gl_FragColor.b == 0.0) discard;
+            }
+
+            if (vClassification == 3.0){
+            // draw the diamond glyph
+                float side_1 = 1.0 - lineSegment(gl_PointCoord, vec2(0.5, 0.05), vec2(0.9, 0.5));
+                float side_2 = 1.0 - lineSegment(gl_PointCoord, vec2(0.9, 0.5), vec2(0.5, 0.95));
+                float side_3 = 1.0 - lineSegment(gl_PointCoord, vec2(0.5, 0.95), vec2(0.1, 0.5));
+                float side_4 = 1.0 - lineSegment(gl_PointCoord, vec2(0.1, 0.5), vec2(0.5, 0.05));
+
+                float shaper = side_1 + side_2 + side_3 + side_4;
+                gl_FragColor = vec4(vec3(shaper) * vColor, gl_FragColor.a);
+                if (gl_FragColor.r + gl_FragColor.g + gl_FragColor.b == 0.0) discard;
+            }
+
+            if (vClassification == 4.0){
+            // draw the plus glyph
+                float vertical = 1.0 - lineSegment(gl_PointCoord, vec2(0.5, 0.05), vec2(0.5, 0.95));
+                float horizontal = 1.0 - lineSegment(gl_PointCoord, vec2(0.05, 0.5), vec2(0.95, 0.5));
+                float shaper = vertical + horizontal;
+                gl_FragColor = vec4(vec3(shaper) * vColor, gl_FragColor.a);
+                if (gl_FragColor.r + gl_FragColor.g + gl_FragColor.b == 0.0) discard;
+            }
+
+            if (vClassification == 5.0){
+            // draw the point glyph
+                if(cc < 0.3){
+                    discard;
+                }
+
+            }
+
+            if (vClassification == 6.0){
+            // draw the square glyph
+            	float top = 1.0 - lineSegment(gl_PointCoord, vec2(0.05, 0.05), vec2(0.95, 0.05));
+                float right = 1.0 - lineSegment(gl_PointCoord, vec2(0.95, 0.05), vec2(0.95, 0.95));
+                float bottom = 1.0 - lineSegment(gl_PointCoord, vec2(0.95, 0.95), vec2(0.05, 0.95));
+                float left = 1.0 - lineSegment(gl_PointCoord, vec2(0.05, 0.95), vec2(0.05, 0.05));
+                float shaper = top + right + bottom + left;
+                gl_FragColor = vec4(vec3(shaper) * vColor, gl_FragColor.a);
+                if (gl_FragColor.r + gl_FragColor.g + gl_FragColor.b == 0.0) discard;
+
+            }
+
+            if (vClassification == 7.0){
+            // draw the star5 glyph
+            	float side = 1.0;  // fragment has side length = 1.0
+                float r = side/2.0;
+                vec2 p = vec2(r);  // center of the fragment
+                float eps = 0.1;   // will be adjusting the tips of the star to avoid clipping
+
+                // Points start at the bottom left tip of the star and move counter clockwise
+                vec2 A = vec2(p.x + 0.618027443 * r, p.y + (1.0 - eps) * r); //
+                vec2 B = vec2(p.x + 0.5 * r, p.y + 0.27637816 * r);
+                vec2 C = vec2(p.x + (1.0 - eps) * r, p.y - 0.236080209 * r);
+                vec2 D = vec2(p.x + 0.309026865 * r, p.y - 0.341634306 * r);
+                vec2 E = vec2(p.x + 0.0 * r, p.y - (1.0 - eps) * r);
+                vec2 F = vec2(p.x - 0.309026865 * r, p.y - 0.341634306 * r);
+                vec2 G = vec2(p.x - (1.0 - eps) * r, p.y - 0.236080209 * r);
+                vec2 H = vec2(p.x - 0.5 * r, p.y + 0.27637816 * r);
+                vec2 I = vec2(p.x - 0.618027443 * r, p.y + (1.0 - eps) * r);
+                vec2 J = vec2(p.x, p.y + 0.658351875 * r);
+                vec2 K = vec2(p.x, p.y + 0.658351875 * r);
+
+                // Draw now the star
+                float line_1  = 1.0 - lineSegment(gl_PointCoord, A, B);
+                float line_2  = 1.0 - lineSegment(gl_PointCoord, B, C);
+                float line_3  = 1.0 - lineSegment(gl_PointCoord, C, D);
+                float line_4  = 1.0 - lineSegment(gl_PointCoord, D, E);
+                float line_5  = 1.0 - lineSegment(gl_PointCoord, E, F);
+                float line_6  = 1.0 - lineSegment(gl_PointCoord, F, G);
+                float line_7  = 1.0 - lineSegment(gl_PointCoord, G, H);
+                float line_8  = 1.0 - lineSegment(gl_PointCoord, H, I);
+                float line_9  = 1.0 - lineSegment(gl_PointCoord, I, J);
+                float line_10 = 1.0 - lineSegment(gl_PointCoord, J, K);
+                float line_11 = 1.0 - lineSegment(gl_PointCoord, K, A);
+
+                float shaper = line_1 + line_2 + line_3 + line_4 + line_5 + line_6 + line_7 + line_8 + line_9 + line_10 + line_11;
+                gl_FragColor = vec4(vec3(shaper) * vColor, gl_FragColor.a);
+                if (gl_FragColor.r + gl_FragColor.g + gl_FragColor.b == 0.0) discard;
+            }
+
+            if (vClassification == 8.0){
+            // draw the star6 glyph
+                float side = 1.0;  // fragment has side length = 1.0
+                float r = side/2.0;
+                vec2 p = vec2(r);  // center of the fragment
+                float eps = 0.05;   // will be adjusting the tips of the star to avoid clipping
+
+                // Points start at the bottom left tip of the star and move clockwise
+                vec2 A = vec2(p.x + 0.50 * r, p.y + 0.87 * r);
+                vec2 B = vec2(p.x, p.y + 0.50 * r);
+                vec2 C = vec2(p.x - 0.50 * r, p.y + 0.87 * r);
+                vec2 D = vec2(p.x - 0.43 * r, p.y + 0.25 * r);
+                vec2 E = vec2(p.x - r, p.y);
+                vec2 F = vec2(p.x - 0.43 * r, p.y - 0.25 * r);
+                vec2 G = vec2(p.x - 0.50 * r, p.y - 0.87 * r);
+                vec2 H = vec2(p.x, p.y - 0.50 * r);
+                vec2 I = vec2(p.x + 0.50 * r, p.y - 0.87 * r);
+                vec2 J = vec2(p.x + 0.43 * r, p.y - 0.25 * r);
+                vec2 K = vec2(p.x + r, p.y);
+                vec2 L = vec2(p.x + 0.43 * r, p.y + 0.25 * r);
+
+                // Draw now the star
+                float line_1  = 1.0 - lineSegment(gl_PointCoord, A, B);
+                float line_2  = 1.0 - lineSegment(gl_PointCoord, B, C);
+                float line_3  = 1.0 - lineSegment(gl_PointCoord, C, D);
+                float line_4  = 1.0 - lineSegment(gl_PointCoord, D, E);
+                float line_5  = 1.0 - lineSegment(gl_PointCoord, E, F);
+                float line_6  = 1.0 - lineSegment(gl_PointCoord, F, G);
+                float line_7  = 1.0 - lineSegment(gl_PointCoord, G, H);
+                float line_8  = 1.0 - lineSegment(gl_PointCoord, H, I);
+                float line_9  = 1.0 - lineSegment(gl_PointCoord, I, J);
+                float line_10 = 1.0 - lineSegment(gl_PointCoord, J, K);
+                float line_11 = 1.0 - lineSegment(gl_PointCoord, K, L);
+                float line_12 = 1.0 - lineSegment(gl_PointCoord, L, A);
+
+
+                float shaper = line_1 + line_2 + line_3 + line_4 + line_5 + line_6 + line_7 + line_8 + line_9 + line_10 + line_11 + line_12;
+                gl_FragColor = vec4(vec3(shaper) * vColor, gl_FragColor.a);
+                if (gl_FragColor.r + gl_FragColor.g + gl_FragColor.b == 0.0) discard;
+            }
+
+            if (vClassification == 9.0){
+            // draw the traingleDown glyph
+                float line_1 = 1.0 - lineSegment(gl_PointCoord, vec2(0.05), vec2(0.5, 0.95));
+                float line_2 = 1.0 - lineSegment(gl_PointCoord, vec2(0.5, 0.95), vec2(0.95, 0.05));
+                float line_3 = 1.0 - lineSegment(gl_PointCoord, vec2(0.95, 0.05), vec2(0.05, 0.05));
+                float shaper = line_1 + line_2 + line_3;
+                gl_FragColor = vec4(vec3(shaper) * vColor, gl_FragColor.a);
+                if (gl_FragColor.r + gl_FragColor.g + gl_FragColor.b == 0.0) discard;
+            }
+
+            if (vClassification == 10.0){
+            // draw the traingleLeft glyph
+            	vec2 uv = (gl_PointCoord - vec2(0.5)) * rot(PI * 0.5); // rotate
+                uv = uv + vec2(0.5);
+                float line_1 = 1.0 - lineSegment(uv, vec2(0.05), vec2(0.5, 0.95));
+                float line_2 = 1.0 - lineSegment(uv, vec2(0.5, 0.95), vec2(0.95, 0.05));
+                float line_3 = 1.0 - lineSegment(uv, vec2(0.95, 0.05), vec2(0.05, 0.05));
+                float shaper = line_1 + line_2 + line_3;
+                gl_FragColor = vec4(vec3(shaper) * vColor, gl_FragColor.a);
+                if (gl_FragColor.r + gl_FragColor.g + gl_FragColor.b == 0.0) discard;
+            }
+
+            if (vClassification == 11.0){
+            // draw the traingleRight glyph
+                vec2 uv = (gl_PointCoord - vec2(0.5)) * rot(1.5 * PI); // rotate
+                uv = uv + vec2(0.5);
+                float line_1 = 1.0 - lineSegment(uv, vec2(0.05), vec2(0.5, 0.95));
+                float line_2 = 1.0 - lineSegment(uv, vec2(0.5, 0.95), vec2(0.95, 0.05));
+                float line_3 = 1.0 - lineSegment(uv, vec2(0.95, 0.05), vec2(0.05, 0.05));
+                float shaper = line_1 + line_2 + line_3;
+                gl_FragColor = vec4(vec3(shaper) * vColor, gl_FragColor.a);
+                if (gl_FragColor.r + gl_FragColor.g + gl_FragColor.b == 0.0) discard;
+            }
+
+            if (vClassification == 12.0){
+            // draw the traingleUp glyph
+            	vec2 uv = (gl_PointCoord - vec2(0.5)) * rot(PI); // rotate
+                uv = uv + vec2(0.5);
+                float line_1 = 1.0 - lineSegment(uv, vec2(0.05), vec2(0.5, 0.95));
+                float line_2 = 1.0 - lineSegment(uv, vec2(0.5, 0.95), vec2(0.95, 0.05));
+                float line_3 = 1.0 - lineSegment(uv, vec2(0.95, 0.05), vec2(0.05, 0.05));
+                float shaper = line_1 + line_2 + line_3;
+                gl_FragColor = vec4(vec3(shaper) * color, gl_FragColor.a);
+                if (gl_FragColor.r + gl_FragColor.g + gl_FragColor.b == 0.0) discard;
+            }
+
         }
 	#endif
 
